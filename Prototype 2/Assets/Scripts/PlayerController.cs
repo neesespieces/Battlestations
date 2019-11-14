97846930995e0b0eb305bd68b59f5116404aeddc
Aneesa Roidad <aneesaroidad@gmail.com>
2019-11-14 09:40:46 -0800

    working on unit 2, prefab animals

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -38,6 +38,11 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
+        if(Input.GetKeyDown(KeyCode.Space)) {
+
+            Instantiate(projectilePrefab, this.transform.position, projectilePrefab.transform.rotation); 
+        }
+
 
         
     }
