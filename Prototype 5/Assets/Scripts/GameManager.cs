125e80ce7046af865b164c1dd9c7ff01663dd7e0
Aneesa Roidad <aneesaroidad@gmail.com>
2019-12-10 09:38:09 -0800

    started prototype 5

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<GameObject> targets; 
+    
+    //Time between objects that are created
+    public float spawnRate = 1.0f;
+
+    public TextMeshProUGUI scoreText;
+
+    private int score; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        StartCoroutine(SpawnTarget());
+        score = 0; 
+        scoreText.text = "Score: " + score; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Every second, random object is selected and created in the game
+    IEnumerator SpawnTarget() {
+
+        while (true) {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+
+
+        }
+    }
+}
---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb; 
+    private float torqueRange = 10;
+
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float xRange = 4;
+    private float yPos = -4;
+
+
+   
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>(); 
+
+        // Objects sent flying up into air at random speed
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse); 
+
+        transform.position = RandomPos(); 
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown() {
+
+        Destroy(this.gameObject);
+    }
+
+    //When the objects pass below a certain point, they hit a box collider that's a trigger
+    // and when this happens, they are deleted from the scene
+    private void OnTriggerEnter (Collider other) {
+
+        Destroy(this.gameObject);
+
+    }
+
+    Vector3 RandomForce() {
+        
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+
+    }
+
+    float RandomTorque() {
+        
+        return Random.Range(-torqueRange, torqueRange);
+
+    }
+
+
+    Vector3 RandomPos() {
+         
+         return new Vector3(Random.Range(-xRange, xRange), yPos);
+
+    }
+}
